a1961f3ccde98b5c855ec29faefd1bc2b45662f8
kafeixiang <79391973+kafeixiang@users.noreply.github.com>
2023-12-09 14:16:41 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -32,7 +32,7 @@ clone_or_update_repo() {
 }
 
 # Array to store repository URLs
-repos=("nekoray=https://github.com/MatsuriDayo/nekoray.git"
+repos=("nekoray=https://github.com/kafeixiang/nekoray.git"
        "v2ray-core=https://github.com/MatsuriDayo/v2ray-core.git")
 
 # Clone or update repositories using the function
